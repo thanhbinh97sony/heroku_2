c80297a8d752dc5fad2962b7928af81e632e54dc
Thanh-Binh <thanhbinhnguyen25091997@gmail.com>
2021-09-05 00:12:08 +0700

    first commit

--- routers/event.js
+++ routers/event.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 const events = require('../controllers/EventController')
 router.get('/events', events.event)
+router.get('/', events.event);
 
 module.exports = router;
